3d8be49addc0f2db9917c67690db43b4a5fd5224
microdopee <sakshilabhane@gmail.com>
2021-01-07 22:34:52 +0530

    Part 1: Plot histogram

---
+++ rprog_data_ProgAssignment3-data/Plot_histogram.R
@@ -0,0 +1,8 @@
+setwd("~/datasciencecoursera/rprog_data_ProgAssignment3-data")
+##Used "read_file" instead of "outcome" to avoid confusion
+read_file<-read.csv("read_file-of-care-measures.csv", colClasses = "character")
+head(read_file)
+ncol(read_file)
+names(read_file)
+read_file[,11]<-as.numeric(read_file[,11])
+hist(read_file[,11])
