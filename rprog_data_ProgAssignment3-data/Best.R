389d84a16a8bf524f74f5aa1279387d2bcc98cc7
microdopee <sakshilabhane@gmail.com>
2021-01-08 20:02:00 +0530

    Corrected Best to final version

--- rprog_data_ProgAssignment3-data/Best.R
+++ rprog_data_ProgAssignment3-data/Best.R
@@ -2,48 +2,44 @@ best<-function(state,outcome)
 {
 #read files and rearrange by states 
   read_file<-read.csv("~/datasciencecoursera/rprog_data_ProgAssignment3-data/outcome-of-care-measures.csv")
-  read_file<-subset(read_file,read_file$State==state)
+  read_file<-subset(read_file[,c(2,11,17,23)],read_file$State==state)
   
 #match state name and subset 
-  if(is.null(read_file))
-    {
-    stop("Invalid state")
-    }
+  if(is.na(read_file[1,])){stop("invalid state")}
+#homogenize data type
+  read_file[,1]<-as.character(read_file[,1])
+  read_file[,c(2,3,4)]<-sapply(read_file[,c(2,3,4)],as.numeric)
 #match by outcome
   if(outcome=="heart attack")
   {
     #row_index of minimum value
-    minimum<-min(read_file[,11], na.rm = TRUE)
+    minimum<-min(read_file[,2], na.rm = TRUE)
     #get row with minimum value
-    row_index<-which(read_file[,11]==minimum)
-  }
-  else if (outcome=="heart failure")
+    row_index<-which(read_file[,2]==minimum)
+  }else if (outcome=="heart failure")
   {
     #row_index of minimum value
-    minimum<-min(read_file[,17], na.rm = TRUE)
+    minimum<-min(read_file[,3], na.rm = TRUE)
     #get row with minimum value
-    row_index<-which(read_file[,17]==minimum)
-  }
-  else if (outcome=="pneumonia")
+    row_index<-which(read_file[,3]==minimum)
+  }else if (outcome=="pneumonia")
   {
     #row_index of minimum value
-    minimum<-min(read_file[,23], na.rm = TRUE)
+    minimum<-min(read_file[,4], na.rm = TRUE)
     #get row with minimum value
-    row_index<-which(read_file[,23]==minimum)
-  }
-  else 
+    row_index<-which(read_file[,4]==minimum)
+  }else 
   {
-    stop("Invalid outcome")
+    stop("invalid outcome")
   }
 #Printing alphabetically
   if(length(row_index)!=1)
   {
-    sorting_data<-read_file[row_index,2]
-    sorted_list<-sort(sorting_data[,2])
+    sorting_data<-read_file[row_index,1]
+    sorted_list<-sort(sorting_data)
     print(sorted_list[1])
-  }
-  else
+  }else
   {
-    print(read_file[row_index,2])
+    print(read_file[row_index,1])
   }
 }
